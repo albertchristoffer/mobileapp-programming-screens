17e040ac4a3d73aa0f39287ef1b93e379de7cd1b
Christoffer <a18chrkr@gmail.com>
2022-04-26 10:28:31 +0200

    Skapade en intent med extras i MainActivity. Tog bort och skapade om de nya filerna på korrekt sätt, blev fel första gången.

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,14 +2,25 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
 
+
+
 public class MainActivity extends AppCompatActivity {
+
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        intent = new Intent(MainActivity.this, MainActivity2.class);
+        intent.putExtra("lorem_ipsum", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+        intent.putExtra("title", "What is Lorem Ipsum?");
+        startActivity(intent);
     }
 
 
